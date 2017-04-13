d4eddc1dec5b27d6573416ba650d06c6612e5964
albar <mubarrid.s@students.amikom.ac.id>
2017-04-13 16:54:23 +0700

    a litle fix

--- app/Comment.php
+++ app/Comment.php
@@ -22,10 +22,11 @@ class Comment extends Model
         $this->newQuery()->distinct()->get([$type])->keyBy($type)->map(function ($model) use ($type) {
             return (new $model->{$type})->getTable();
         })->each(function ($table, $modelClass) use (&$query, $type, $key, $callable) {
-            $model = new $modelClass;
+            $query = $query->orWhereExists(function ($query) use ($table, $modelClass, $type, $key, $callable) {
+                $model = new $modelClass;
 
-            $query = $query->orWhereExists(function ($query) use ($table, $modelClass, $model, $type, $key, $callable) {
-                $query->select('*')->from($table)->where("{$this->getTable()}.{$type}", $modelClass)->whereRaw("{$this->getTable()}.{$key} = {$table}.{$model->getKeyName()}")
+                $query->select('*')->from($table)->where("{$this->getTable()}.{$type}", $modelClass)
+                    ->whereRaw("{$this->getTable()}.{$key} = {$table}.{$model->getKeyName()}")
                     ->when($callable instanceof \Closure, $callable);
             });
         });
