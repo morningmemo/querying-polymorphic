5eeeb0722afcbc290cc7b1b4ea28e30ea52df793
albar <mubarrid.s@students.amikom.ac.id>
2017-04-19 13:46:03 +0700

    fix polymorphic qury

--- app/Comment.php
+++ app/Comment.php
@@ -17,23 +17,23 @@ class Comment extends Model
 
     public function scopeWhereHasCommentable($query, $callable = null)
     {
-        list($type, $key) = $this->getPolymorphicFields();
+        return $query->where(function ($query) use ($callable) {
+            list($type, $key) = $this->getPolymorphicFields();
 
-        $this->newQuery()->distinct()->get([$type])->keyBy($type)->map(function ($model) use ($type) {
-            return (new $model->{$type})->getTable();
-        })->each(function ($table, $modelClass) use (&$query, $type, $key, $callable) {
-            $query = $query->orWhereExists(function ($query) use ($table, $modelClass, $type, $key, $callable) {
-                $model = new $modelClass;
+            $this->newQuery()->distinct()->get([$type])->keyBy($type)->map(function ($model) use ($type) {
+                return (new $model->{$type})->getTable();
+            })->each(function ($table, $modelClass) use ($query, $type, $key, $callable) {
+                $query->orWhereExists(function ($query) use ($table, $modelClass, $type, $key, $callable) {
+                    $model = new $modelClass;
 
-                $query->select('*')->from($table)->where("{$this->getTable()}.{$type}", $modelClass)
-                    ->whereRaw("{$this->getTable()}.{$key} = {$table}.{$model->getKeyName()}")
-                    ->when($callable instanceof \Closure, function ($query) use ($callable) {
-                        $query->where($callable);
-                    });
+                    $query->select('*')->from($table)->where("{$this->getTable()}.{$type}", $modelClass)
+                        ->whereRaw("{$this->getTable()}.{$key} = {$table}.{$model->getKeyName()}")
+                        ->when($callable instanceof \Closure, function ($query) use ($callable) {
+                            $query->where($callable);
+                        });
+                });
             });
         });
-
-        return $query;
     }
 
     protected function getPolymorphicFields()
